e469eb2c53f6b3aea617a5e464b224eb8db70d6e
FabianBrinkmann <fabian.brinkmann218@yahoo.de>
2015-04-28 11:15:25 +0200

    OptionPanels

--- src/de/szut/SmartGadgetBar/GUI/AbstractWidgetPanel.java
+++ src/de/szut/SmartGadgetBar/GUI/AbstractWidgetPanel.java
@@ -1,8 +1,24 @@
 package de.szut.SmartGadgetBar.GUI;
 
-public class AbstractWidgetPanel {
-	private String widgetName;
-	public String getName() {
-		return widgetName;
+import java.util.Properties;
+
+public abstract class AbstractWidgetPanel {
+	private WidgetInterface widget;
+	private JPanel panel;
+	
+	
+	
+	public AbstractWidgetPanel(WidgetInterface parent){
+		this.widget = parent;
 	}
+	public String getWidget() {
+		return widget;
+	}
+	
+	public void optionClicked(){
+		new OptionPanelWidget(widget);
+	}
+	
+	 abstract void initializePanel();
+	
 }
