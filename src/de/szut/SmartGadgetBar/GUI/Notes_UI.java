71bd92a9ce86f3cfb791eef5193f9ffeaf68e431
Simeon <Simeon@Simeon-PC>
2015-05-18 22:24:02 +0200

    - Notes

--- src/de/szut/SmartGadgetBar/GUI/Notes_UI.java
+++ src/de/szut/SmartGadgetBar/GUI/Notes_UI.java
@@ -3,6 +3,9 @@ package de.szut.SmartGadgetBar.GUI;
 import java.awt.Rectangle;
 import java.io.File;
 
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextArea;
 
 import de.szut.SmartGadgetBar.Model.WidgetInterface;
@@ -19,8 +22,13 @@ public class Notes_UI extends AbstractWidgetPanel {
 	void initializePanel() {
 		setLayout(null);
 		JTextArea textfield = new JTextArea();
-		textfield.setBounds(0, 10, 280, 60);
+		textfield.setBounds(0, 7, 280, 66);
 		add(textfield);
+		
+		JPopupMenu popupMenu = new JPopupMenu();
+		JMenuItem saveDatabase = new JMenuItem("In Datenbank Speichern");
+		JMenuItem saveTxt= new JMenuItem("Speichern als .txt");
+		
 		setVisible(true);
 	}
 	
